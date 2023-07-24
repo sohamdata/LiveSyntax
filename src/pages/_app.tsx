5d88cfe8ee675e0927e778cebd13cb8cd308b0f7
Soham Datta <sohamdatta34@gmail.com>
2023-07-24 23:59:42 +0530

    chore: moved metadata to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>LiveSyntax</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="description" content="LiveSyntax: A live code editor for pair programming" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,13 @@
 import JoinRoom from "@/components/JoinRoom";
 import useHasMounted from "@/utils/hooks/useHasMounted";
-import Head from "next/head";
 
 export default function Home() {
   const hasMounted = useHasMounted();
   if (!hasMounted) return null;
 
   return (
-    <>
-      <Head>
-        <title>LiveSyntax</title>
-        <meta name='description' content='Code with your friends in real time!' />
-      </Head>
-      <main className="bg-dark-choco h-screen w-screen">
-        <JoinRoom />
-      </main>
-    </>
+    <main className="bg-dark-choco h-screen w-screen">
+      <JoinRoom />
+    </main>
   )
 }
