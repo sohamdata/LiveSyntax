5473ad1be52d84732e4a21a2859b44368468c70d
Soham Datta <sohamdatta34@gmail.com>
2023-07-25 22:35:45 +0530

    init: server

--- client/src/components/TextEditor.tsx
+++ client/src/components/TextEditor.tsx
@@ -5,6 +5,24 @@ import { javascript } from '@codemirror/lang-javascript';
 const TextEditor = () => {
     return (
         <CodeMirror
+            value={tempCode}
+            style={{ color: 'black' }}
+            extensions={[javascript()]}
+            theme={tokyoNightStorm}
+            height="100vh"
+        />
+    )
+}
+
+export default TextEditor;
+
+const tempCode = `import CodeMirror from '@uiw/react-codemirror';
+import { tokyoNightStorm } from "@uiw/codemirror-theme-tokyo-night-storm";
+import { javascript } from '@codemirror/lang-javascript';
+
+const TextEditor = () => {
+    return (
+        <CodeMirror
             value={"universal universe"}
             style={{ color: 'black' }}
             extensions={[javascript()]}
@@ -15,3 +33,4 @@ const TextEditor = () => {
 }
 
 export default TextEditor;
+`;
---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const PORT = process.env.PORT || 3001;
+
+io.on('connection', (socket) => {
+    console.log('a user connected, like the 4th of July ');
+    console.log(socket);
+});
+
+server.listen(PORT, () => { console.log(`listening on port ${PORT} like the 4th of July`); });
