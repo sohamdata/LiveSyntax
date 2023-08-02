49d60d944493b344a71558123965a2377587510b
Soham Datta <sohamdatta34@gmail.com>
2023-08-03 00:56:42 +0530

    added cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const cors = require('cors');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 
+app.use(cors());
 const io = new Server(server);
 const PORT = process.env.PORT || 5000;
 
