0e81e5f00660030af905fff35628f6cf2d240c94
Soham Datta <sohamdatta34@gmail.com>
2023-08-10 02:17:41 +0530

    ui: added server messages

--- client/src/components/JoinRoom.tsx
+++ client/src/components/JoinRoom.tsx
@@ -82,13 +82,13 @@ const JoinRoom = () => {
                     <div>
                         {!isGenerated && <p className="text-center text-white">
                             Don't have a room?
-                            <span className="ml-2 text-blue-600 cursor-pointer hover:underline" onClick={generateRoomId}>
+                            <span className="ml-2 text-green-500 cursor-pointer hover:underline" onClick={generateRoomId}>
                                 Create one
                             </span>
                         </p>}
                         {isGenerated && <p className="text-center text-white">
                             join another room?
-                            <span className="ml-2 text-gray-400 cursor-pointer hover:underline" onClick={joinAnotherRoom}>
+                            <span className="ml-2 text-blue-400 cursor-pointer hover:underline" onClick={joinAnotherRoom}>
                                 enter a room id
                             </span>
                         </p>
@@ -96,24 +96,43 @@ const JoinRoom = () => {
                     </div>
                 </form>
             </div>
-            <div className='flex flex-col justify-center items-center p-6 mt-4 max-w-md w-full bg-gray-800 rounded-lg shadow-md'>
+            <div className={`flex flex-col justify-center items-center p-6 mt-4 max-w-md w-full bg-gray-800 rounded-lg shadow-md ${isServerExpanded && 'shadow-tesla-red'}`}>
                 <button
                     onClick={() => setIsServerExpanded(!isServerExpanded)}
-                    className='w-[300px] py-2 text-white bg-tesla-blue rounded-md hover:bg-tesla-red transition duration-300'
-                >
-                    Server Error?
+                    className='w-[300px] py-2 text-white bg-slate-900 rounded-md hover:bg-indigo-900 transition duration-300 outline-none'
+                > more
                 </button>
                 {isServerExpanded && (
-                    <div className='flex flex-col justify-center items-center'>
-                        <button
-                            className='mt-5 w-[300px] py-2 text-white bg-green-500 rounded-md hover:bg-green-600 transition duration-300'
-                            onClick={startServer}
-                        >
-                            &gt; Wakeup Server
-                        </button>
-                        <p className='mt-5 text-teal-700'>
-                            glitch.com puts the server to sleep after just 5 minutes of inactivity
-                        </p>
+                    <div>
+                        <div className='mt-5 flex justify-between items-center '>
+                            <div className='mr-5 flex flex-col text-center'>
+                                <p className='text-red-500 font-semibold'>Server down?</p>
+                                <p className='mt-1 text-slate-200 text-sm w-[200px]'>
+                                    glitch.com puts the server to sleep after just 5 minutes of inactivity {':('}
+                                </p>
+                            </div>
+                            <button
+                                className='animate-pulse py-2 px-4 text-white bg-green-500 rounded-md hover:bg-green-600 transition'
+                                onClick={startServer}
+                            >
+                                Wakeup Server
+                            </button>
+                        </div>
+                        <div className='mt-5 flex justify-between items-center '>
+                            <div className='mr-5 flex flex-col text-center'>
+                                <p className='text-red-500 font-semibold'>Found a bug?</p>
+                                <p className='mt-1 text-slate-200 text-sm w-[200px]'>
+                                    Please report it on github
+                                </p>
+                            </div>
+                            <a href='https://github.com/sohamdata/LiveSyntax/' target='_blank' rel='noreferrer'>
+                                <button
+                                    className='py-2 px-4 text-white bg-green-500 rounded-md hover:bg-green-600 transition duration-300'
+                                >
+                                    GitHub Repo
+                                </button>
+                            </a>
+                        </div>
                     </div>
                 )}
             </div>
